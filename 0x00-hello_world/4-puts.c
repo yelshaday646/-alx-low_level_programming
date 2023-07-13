3e0f826d520f0dc4f38ab71bcb41f0df42aa5e48
zozi <zeeiikibru16@gmail.com>
2023-07-13 14:14:58 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+{
+	puts("\"programing is like building a multilingual puzzle");
+	return 0
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+{
+	printf("with proper grammar,but the outcom is piec of art,\n");
+	return (0);
