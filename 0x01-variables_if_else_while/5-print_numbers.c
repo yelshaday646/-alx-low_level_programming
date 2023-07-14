2d4c68523d9cb0e5f5dc34b38c9f89395c5bb7cb
zozi <zeeiikibru16@gmail.com>
2023-07-14 13:32:01 -0700

    variables and if statment

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers from 0 and 9.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        int i;
+        for (i = 0; i < 10; i++)
+        {
+                printf("%d" , i);
+        }
+        putchar('\n');
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+task
+0x01. C - Variables, if, else, while
