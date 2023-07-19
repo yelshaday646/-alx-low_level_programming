4056e3537152a5931ad3b1171b167861488385c2
zozi <zeeiikibru16@gmail.com>
2023-07-19 01:48:47 -0700

    my second commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 /**
  * print_sign - print the sign of a number
  * @n: the number to be checked
+ * Return: 1 for postive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
