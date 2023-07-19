0b150add5dcc704e31963de165c390bb98b7f185
zozi <zeeiikibru16@gmail.com>
2023-07-19 01:45:23 -0700

    my second commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,25 @@
 #include "main.h"
+/**
+ * print_sign - print the sign of a number
+ * @n: the number to be checked
+ */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} 
-	else if (n == 0)
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
